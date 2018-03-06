ce78f0db53da51dc2df39dc83fd950200ea41f9c
crutch_lord <slonik256@yandex.ru>
2018-03-06 20:01:55 +0300

    added iss54.py

---
+++ iss54.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def func():
+	print("this is func")
+	
+class thing():
+	def _init_(self):
+		pass
+	def my_func(self):
+		print("this is func")
+	
+if __name__ == __main__ :
+	func()
+ 
